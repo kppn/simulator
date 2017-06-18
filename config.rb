f908f40e57ed0ead54c6b4a0bef699653e4df0d9
kppn <tkondoh58@gmail.com>
2017-06-19 06:31:36 +0900

    g

--- config.rb
+++ config.rb
@@ -3,6 +3,8 @@ log 'LoRa Simulator'
 #require_relative '/home/ta_kondoh/work/device_gw_simulator/lib/lora/protocol.rb'
 #require_relative '/home/ta_kondoh/work/device_gw_simulator/lib/packet_forwarder/protocol.rb'
 
-peer '127.0.0.1:50000', '127.0.0.1:50001', String
+require_relative '/home/ta_kondoh/work/simlator/protocol/raw'
+
+peer '127.0.0.1:50000', '127.0.0.1:50001', Raw
 #peer :other, '127.0.0.1:50002', '127.0.0.1:50003', XProto
 
